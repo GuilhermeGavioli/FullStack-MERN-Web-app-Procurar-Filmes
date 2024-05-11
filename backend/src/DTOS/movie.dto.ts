3a681c47da8adb3bb1dc32685a989d0f7dbd3554
unknown <xiiguilhermeiix@gmail.com>
2024-05-11 18:50:20 -0300

    building movie dto

--- backend/src/DTOS/movie.dto.ts
+++ backend/src/DTOS/movie.dto.ts
@@ -1,8 +1,24 @@
+interface Director{
+    name: string,
+    picture: string,
+}
+
+interface Actor{
+    name: string,
+    picture: string,
+}
+
 export class Movie{
     constructor(
         readonly title: string,
         readonly cover: string,
+        readonly genres: string[],
         readonly description: string,
+        readonly runTime: number,
+        readonly released: string,
+        readonly director: Director[],
+        readonly actors: Actor[],
+        
     ){}
 }
 
