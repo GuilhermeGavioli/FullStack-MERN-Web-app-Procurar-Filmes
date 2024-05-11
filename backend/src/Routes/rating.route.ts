dc532e258b46c5054719757e90052a472c146593
unknown <xiiguilhermeiix@gmail.com>
2024-05-11 20:58:14 -0300

    middlewares

--- backend/src/Routes/rating.route.ts
+++ backend/src/Routes/rating.route.ts
@@ -1,6 +1,6 @@
 import { router } from "../main";
 import { ratingController } from "../Objects";
-import { AuthGuard } from "../Auth.guard";
+import { AuthGuard } from "../Middlewares/authguard.middleware";
 
 router.post('/ratings/create/for_movie/:movie_id', AuthGuard.verifyToken, (rq:any,rs:any) => ratingController.createRating(rq,rs))
 router.get('/ratings/delete/:id', AuthGuard.verifyToken, (rq:any,rs:any) => ratingController.deleteRating(rq,rs))
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,9 @@
 
-import swaggerUi from 'swagger-ui-express'
+
 import express from 'express'
+
 import {OAuth2Client}  from 'google-auth-library'
-import { specs } from './swagger.jsdoc'
-import cors from 'cors'
-
 // const client = new OAuth2Client(process.env.GOOGLE_OAUTH_CLIENT_ID, process.env.GOOGLE_OAUTH_SECRET_KEY)
-
-
 
 
 import { Database } from './database';
@@ -24,36 +20,11 @@ async function Connect(){
 
 
 Connect();
-
 export { db };
-
-
-
-
-
-
-
-
 
 
 const app = express()
 export const router = express.Router()
-
-// Global Middlewares
-router.use(router)
-router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
-router.use(cors({origin: '*', allowedHeaders: '*'}))
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(3001, () => {
     console.log('Server is Up and Running')
